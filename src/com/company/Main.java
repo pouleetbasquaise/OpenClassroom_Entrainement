226b79982cad02bffd3e48fcdd56cf5de701d698
pouleetbasquaise <65782158+pouleetbasquaise@users.noreply.github.com>
2021-05-23 12:34:23 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+class animal {
+
+   void deplacer() {
+	System.out.println("Je me deplace");
+    }
+}
+
+
+
+class chien extends animal{
+
+
+    void deplacer() {
+        System.out.println("Je marche");
+    }
+}
---
+++ src/com/company/Test.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] arg) {
+
+        animal a1 = new animal();
+
+        animal a2 = new chien();
+
+        a1.deplacer();
+
+        a2.deplacer();
+    }
+}
