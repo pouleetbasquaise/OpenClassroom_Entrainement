46a349c9c6678f3fdc791c9d450beab82b5d8a70
pouleetbasquaise <65782158+pouleetbasquaise@users.noreply.github.com>
2021-05-28 10:43:27 +0200

    Hello World en Java

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -1,9 +1,9 @@
 package hello;
 
+//Class qui pourra être repris par des extend de class dans des packages similaire
 public class HelloWorld {
     public HelloWorld() {
     }
-
     public static void main(String[] args) {
         System.out.println("Hello World !");
     }
